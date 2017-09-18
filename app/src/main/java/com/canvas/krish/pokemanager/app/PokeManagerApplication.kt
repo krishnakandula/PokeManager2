137967e2740eebfc279622a13c57dc6e074a3356
Krishna Kandula <krishna.c.kandula@gmail.com>
2017-09-18 09:33:29 -0500

    Add Application class

--- app/src/main/java/com/canvas/krish/pokemanager/app/PokeManagerApplication.kt
+++ app/src/main/java/com/canvas/krish/pokemanager/app/PokeManagerApplication.kt
@@ -1,5 +1,19 @@
 package com.canvas.krish.pokemanager.app
 
+import android.app.Application
+
 /**
  * Created by Krishna Chaitanya Kandula on 9/16/2017.
  */
+class PokeManagerApplication : Application() {
+
+    val component: PokeManagerAppComponent by lazy {
+        DaggerPokeManagerAppComponent.builder()
+                .pokeManagerAppModule(PokeManagerAppModule(this))
+                .build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
