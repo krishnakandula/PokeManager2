e6e55457fb8479b205a09b0038415a40f69fae33
Krishna Kandula <krishna.c.kandula@gmail.com>
2017-09-18 19:32:51 -0500

    Merge branch 'master' into dev

--- app/src/main/java/com/canvas/krish/pokemanager/data/source/PokemonRepository.kt
+++ app/src/main/java/com/canvas/krish/pokemanager/data/source/PokemonRepository.kt
@@ -2,13 +2,14 @@ package com.canvas.krish.pokemanager.data.source
 
 import com.canvas.krish.pokemanager.data.models.Pokemon
 import com.canvas.krish.pokemanager.data.models.PokemonListResult
+import io.reactivex.Single
 
 /**
  * Created by Krishna Chaitanya Kandula on 9/16/2017.
  */
 interface PokemonRepository {
 
-    fun getPokemonList(offset: Int, limit: Int, onSuccess: (List<PokemonListResult>) -> Unit, onError: (t: Throwable?) -> Unit)
+    fun getPokemonList(offset: Int, limit: Int): Single<List<PokemonListResult>>
 
     fun getPokemon(id: Int, onSuccess: (Pokemon) -> Unit, onError: (t: Throwable?) -> Unit)
 
