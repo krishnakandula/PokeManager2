3e3a8993eb200da7cbe4fa75d2e22dec2b68d889
krishnakandula <krishna.c.kandula@gmail.com>
2017-09-21 13:30:07 -0500

    Remove palette json generation

--- app/src/main/java/com/canvas/krish/pokemanager/ui/pokemonlist/PokemonListViewHolder.kt
+++ app/src/main/java/com/canvas/krish/pokemanager/ui/pokemonlist/PokemonListViewHolder.kt
@@ -1,21 +1,15 @@
 package com.canvas.krish.pokemanager.ui.pokemonlist
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.drawable.GradientDrawable
-import android.support.v7.graphics.Palette
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import com.canvas.krish.pokemanager.R
 import com.canvas.krish.pokemanager.data.models.PokemonListResult
 import com.squareup.picasso.Picasso
-import io.reactivex.Single
 import kotlinx.android.synthetic.main.itemview_pokemon_list.view.*
 import org.json.JSONArray
-import org.json.JSONException
-import org.json.JSONObject
-import java.io.*
 
 /**
  * Created by Krishna Chaitanya Kandula on 9/18/17.
@@ -44,65 +38,17 @@ class PokemonListViewHolder(itemView: View, private val context: Context) : Recy
 
         if(pokemonListResult.palette != null) {
             itemView.cardview_PokemonListItemView.setCardBackgroundColor(pokemonListResult.palette.lightMuted)
+            itemView.pokemonImageView_pokemonListItemView.borderColor = pokemonListResult.palette.vibrant
         }
         changeTextColors(context.getColor(R.color.md_white_1000))
         changeOutlineColors(context.getColor(R.color.md_white_1000))
+
 
         Picasso.with(context)
                 .load(pokemonListResult.imageUrl)
                 .resizeDimen(R.dimen.minPokemonImageViewDiameter, R.dimen.minPokemonImageViewDiameter)
                 .centerInside()
                 .into(itemView.pokemonImageView_pokemonListItemView)
-    }
-
-    fun storeColorPalette(id: Int, palette: Palette) {
-        try {
-            val jsonArray: JSONArray? = parseInitialData(context)
-            if (jsonArray != null) {
-                val jsonObject: JSONObject = jsonArray.getJSONObject(id - 1)
-                if (!jsonObject.has("-color")) {
-
-                    val colorJsonObj: JSONObject = JSONObject()
-                    colorJsonObj.put("_lightMuted", palette.getLightMutedColor(context.getColor(R.color.primary)))
-                    colorJsonObj.put("_darkMuted", palette.getDarkMutedColor(context.getColor(R.color.primary)))
-                    colorJsonObj.put("_dominant", palette.getDominantColor(context.getColor(R.color.primary)))
-                    colorJsonObj.put("_darkVibrant", palette.getDarkVibrantColor(context.getColor(R.color.primary)))
-                    colorJsonObj.put("_lightVibrant", palette.getLightVibrantColor(context.getColor(R.color.primary)))
-                    colorJsonObj.put("_muted", palette.getMutedColor(context.getColor(R.color.primary)))
-                    colorJsonObj.put("_vibrant", palette.getVibrantColor(context.getColor(R.color.primary)))
-                    jsonObject.put("_color", colorJsonObj)
-                    dataArray = jsonArray
-
-                    if(id == 151) {
-                        Log.d(LOG_TAG, "f")
-                    }
-                    numberOfWrittenColors++
-                    Log.d(LOG_TAG, "$numberOfWrittenColors")
-                    if (numberOfWrittenColors == 151) {
-                        val file: File = File("${context.filesDir.absolutePath}colors_data.json")
-                        Log.d(LOG_TAG, context.filesDir.absolutePath)
-                        val output: Writer = BufferedWriter(FileWriter(file))
-                        output.write(jsonArray.toString())
-                        output.close()
-                    }
-                }
-            }
-        } catch (e: JSONException) {
-        }
-    }
-
-    private fun parseInitialData(context: Context): JSONArray? {
-        if (dataArray == null) {
-            val inputStream: InputStream = context.assets.open(INITIAL_DATA_PATH)
-            val size: Int = inputStream.available()
-            val buffer = ByteArray(size)
-            inputStream.read(buffer)
-            inputStream.close()
-
-            val jsonData = String(buffer)
-            dataArray = JSONArray(jsonData)
-        }
-        return dataArray
     }
 
     private fun changeTextColors(color: Int) {
@@ -123,12 +69,5 @@ class PokemonListViewHolder(itemView: View, private val context: Context) : Recy
         (itemView.pokemonType2TextView_pokemonListItemView.background as GradientDrawable).setStroke(
                 context.resources.getDimension(R.dimen.defaultOutlineStrokeWidth).toInt(),
                 color)
-    }
-
-    private fun generateColorPalette(bitmap: Bitmap): Single<Palette> {
-        return Single.create { emitter ->
-            val palette = Palette.from(bitmap).generate()
-            emitter.onSuccess(palette)
-        }
     }
 }
